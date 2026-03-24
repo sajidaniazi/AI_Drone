1189b78d85863f74545e8f28e418cf9f5b856128
C00282704 <C00282704@setu.ie>
2026-03-24 14:55:39 +0000

    Red_Lego Images and their lables

--- cameraTest.py
+++ cameraTest.py
@@ -1,34 +1,52 @@
+#Code from: https://dipankarmedh1.medium.com/real-time-object-detection-with-yolo-and-webcam-enhancing-your-computer-vision-skills-861b97c78993
+
 from ultralytics import YOLO
 import cv2
-
+import math
+# start webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-model = YOLO("C:\\Users\\C00282704\\PycharmProjects\\PythonProject2\\runs\\classify\\train12\\weights\\best.pt")
+# model
+model = YOLO("C:/Users/C00282704/PycharmProjects/PythonProject2/runs/detect/train11/weights/best.pt")
 
+# object classes
 classNames = ["Red_Lego"]
+
 
 while True:
     success, img = cap.read()
-    if not success:
-        break
-
     results = model(img, stream=True)
 
+    # coordinates
     for r in results:
-        probs = r.probs  # Classification probabilities
+        boxes = r.boxes
 
-        if probs is not None:
-            top_class_index = int(probs.top1)          # Index of top predicted class
-            confidence = round(float(probs.top1conf), 2)  # Confidence score
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            label = f"{classNames[top_class_index]}: {confidence}"
-            print(label)
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            # Display label on frame
-            cv2.putText(img, label, (20, 40), cv2.FONT_HERSHEY_SIMPLEX,
-                        1, (255, 0, 0), 2)
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
