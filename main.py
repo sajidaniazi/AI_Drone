08ea15c8b15d4f840481ecb7e225feae0b87cf5f
C00282704 <C00282704@setu.ie>
2026-03-24 15:50:16 +0000

    updated code

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load a pretrained YOLO26n model
 model = YOLO("yolov8n.yaml")
 
-results = model.train(data="classification.yaml", epochs=60)
+results = model.train(data="classification.yaml", epochs=1)
