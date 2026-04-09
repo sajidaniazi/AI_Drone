cbb3383f5575ffcc21611b0c87981d362da8d331
C00282704 <C00282704@setu.ie>
2026-04-09 17:35:08 +0100

    New images and val

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load a pretrained YOLO26n model
 model = YOLO("yolov8n.pt")
 
-results = model.train(data="classification.yaml", epochs=60)
+results = model.train(data="classification.yaml", epochs=60, hsv_h=0.03, hsv_s=0.6, hsv_v=0.5)
