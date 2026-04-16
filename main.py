0f70ce6e694ba134c71231da2377020984916f62
C00282704 <C00282704@setu.ie>
2026-04-16 17:31:00 +0100

    New training model and data

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load a pretrained YOLO26n model
 model = YOLO("yolov8n.pt")
 
-results = model.train(data="classification.yaml", epochs=60, hsv_h=0.03, hsv_s=0.6, hsv_v=0.5)
+results = model.train(data="dataset.yaml", epochs=60, batch=16, hsv_h=0.015, hsv_s=0.7, hsv_v=0.4, fliplr=0.5, flipud=0.1, degrees=15, translate=0.1, scale=0.5, shear=5.0,mosaic=1.0, mixup=0.1, copy_paste=0.1, name="red_lego_detector")
